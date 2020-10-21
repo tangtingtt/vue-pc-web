806822288e248ee61541083b324c4d9d7e19c6c4
ting.tang <ting.tang@chinacreator.com>
2020-10-21 22:12:55 +0800

    拖拽组件添加

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,70 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <img alt="Vue logo" src="../assets/logo.png"> -->
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+      <grid-layout
+        :layout="layoutData"
+        :col-num="12"
+        :row-height="layoutHeight"
+        :is-draggable="dialogVisible"
+        :is-resizable="dialogVisible"
+        :is-mirrored="false"
+        :vertical-compact="true"
+        :margin="[10, 10]"
+        :use-css-transforms="true"
+       >
+         <grid-item v-for="(item,index) in layoutData"
+                    :x="item.x"
+                    :y="item.y"
+                    :w="item.w"
+                    :h="item.h"
+                    :i="item.i"
+                    :key="item.i"
+         >
+           {{index}}
+         </grid-item>
+       </grid-layout>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import layoutData from '@/assets/json/layoutData.json'
+// import HelloWorld from '@/components/HelloWorld.vue'
+import VueGridLayout from 'vue-grid-layout'
+
+const GridLayout = VueGridLayout.GridLayout
+const GridItem = VueGridLayout.GridItem
 
 export default {
   name: 'Home',
+  data () {
+    return {
+      // 布局数据
+      layoutData: [],
+      layoutConfig: {
+        height: 100, // 默认高度
+        dialogVisible: false // 是否可拖拽或改变大小
+      }
+    }
+  },
   components: {
-    HelloWorld
+    // HelloWorld,
+    GridLayout,
+    GridItem
+  },
+  methods: {
+    init () {
+      this.layoutData = layoutData.mainData
+    }
+  },
+  created () {
+    this.init()
   }
 }
 </script>
+<style scoped>
+.vue-grid-item {
+  background: aquamarine;
+}
+</style>
